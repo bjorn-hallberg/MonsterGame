9e256ae5ea733254b47fea7f2c593f121ca42754
Björn Hallberg <bjorn.hallberg@hm.com>
2020-10-07 17:07:04 +0200

    Create terminal

--- src/main/java/MonsterGame.java
+++ src/main/java/MonsterGame.java
@@ -1,21 +1,39 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class MonsterGame {
 
-//    private Player player;
-//    private List<Monster> monsters;
-//    private List<Obstacles> obstacles;
-//    private List<Bomb> bombs;
+    static private Terminal terminal;
+    static private TextGraphics tg;
+
+//    static private Player player;
+//    static private List<Monster> monsters;
+//    static private List<Obstacles> obstacles;
+//    static private List<Bomb> bombs;
+
 
     public static void main(String[] args) {
-        startGame();
+        try {
+            startGame();
+            endGame();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void startGame() {
+    private static void startGame() throws IOException {
+        // Create terminal
+        terminal = new DefaultTerminalFactory().createTerminal();
+        tg = terminal.newTextGraphics();
+        terminal.setCursorVisible(false);
 
+        initGame();
     }
 
     private static void initGame() {
-        // Create terminal
-
         // Create player
 
         // Create monsters
@@ -32,5 +50,10 @@ public class MonsterGame {
 
     private static void moveMonsters() {
 
+    }
+
+    private static void endGame() throws IOException, InterruptedException {
+        Thread.sleep(5000);
+        terminal.close();
     }
 }
