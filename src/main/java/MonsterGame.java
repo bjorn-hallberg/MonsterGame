48da9618c87c278f7e7edadfbf92e4b28f825555
Björn Hallberg <bjorn.hallberg@hm.com>
2020-10-07 16:40:23 +0200

    Added class MonsterGame

---
+++ src/main/java/MonsterGame.java
@@ -0,0 +1,36 @@
+public class MonsterGame {
+
+//    private Player player;
+//    private List<Monster> monsters;
+//    private List<Obstacles> obstacles;
+//    private List<Bomb> bombs;
+
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    private static void startGame() {
+
+    }
+
+    private static void initGame() {
+        // Create terminal
+
+        // Create player
+
+        // Create monsters
+
+        // Create obstacles
+
+        // Create bombs
+
+    }
+
+    private static void movePlayer() {
+
+    }
+
+    private static void moveMonsters() {
+
+    }
+}
