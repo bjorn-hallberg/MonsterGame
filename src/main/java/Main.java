57ac821a5d0905dd32bca0545650db6191677e9e
HM\IVRIV <ivan.larsson@hm.com>
2020-10-08 09:46:15 +0200

    Created Obstacle class Removed obstacle related code from Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,26 +35,6 @@ public class Main {
         tg.setForegroundColor(TextColor.ANSI.YELLOW);
         tg.putString(monsterX, monsterY, String.valueOf(monster));
 
-        // Create obstacles
-        List<TerminalPosition> obstacles = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            obstacles.add(new TerminalPosition(10 + i, 10));
-        }
-        for (int i = 0; i < 6; i++) {
-            obstacles.add(new TerminalPosition(30, 8 + i));
-        }
-        for (int i = 0; i < 12; i++) {
-            obstacles.add(new TerminalPosition(58 + i, 14));
-        }
-        for (int i = 0; i < 8; i++) {
-            obstacles.add(new TerminalPosition(46, 12 + i));
-        }
-
-        // Print obstacles
-        for (TerminalPosition p : obstacles) {
-            tg.setForegroundColor(TextColor.ANSI.CYAN);
-            tg.putString(p, String.valueOf(block));
-        }
 
         // Create and print bomb
         Random rnd = new Random();
