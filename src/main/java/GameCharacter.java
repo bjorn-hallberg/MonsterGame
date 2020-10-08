e9d846c7a1034d436b0a50dd8a63151f97fa9ac1
Björn Hallberg <bjorn.hallberg@hm.com>
2020-10-09 01:32:36 +0200

    Added abstract class GameCharacter

---
+++ src/main/java/GameCharacter.java
@@ -0,0 +1,21 @@
+public abstract class GameCharacter extends GameObject {
+    protected int previousX;
+    protected int previousY;
+
+    public GameCharacter(int x, int y) {
+        super(x, y, "☻");
+        previousX = x;
+        previousY = y;
+    }
+
+    public GameCharacter(int x, int y, String symbol) {
+        super(x, y, symbol);
+        previousX = x;
+        previousY = y;
+    }
+
+    public void moveToPreviousPosition() {
+        x = previousX;
+        y = previousY;
+    }
+}
