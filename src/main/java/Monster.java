42b888678ee6206a8fccbd811f0e78d812e8db1e
Björn Hallberg <bjorn.hallberg@hm.com>
2020-10-09 09:50:27 +0200

    Validate monsters moves

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,10 +1,10 @@
-public class Monster extends GameCharacter{
+public class Monster extends GameCharacter {
 
     public Monster(int x, int y) {
-        super(x,y,"⚉");
+        super(x, y, "⚉");
     }
 
-    public void moveMonster(Player player){
+    public void moveMonster(Player player) {
         // The monster moves towards the player based on players move
         // Save monster's old position
         previousX = x; // save old position x
@@ -12,17 +12,16 @@ public class Monster extends GameCharacter{
 
         // Calculate monster's new position
         if (player.getX() < x) {
-                x--;
-            } else if (player.getX() > x) {
-                x++;
-            }
-            if (player.getY() < y) {
-                y--;
-            } else if (player.getY() > y) {
-                y++;
-            }
+            x--;
+        } else if (player.getX() > x) {
+            x++;
+        }
+        if (player.getY() < y) {
+            y--;
+        } else if (player.getY() > y) {
+            y++;
+        }
 
     }
-
 
 }
