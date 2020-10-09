bb168f5ad345b7f8d8d5bb66ad5046821419fba3
HM\MAMIS <maria.mickols@hm.com>
2020-10-09 09:22:05 +0200

    Adapted Monster class to super class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,50 +1,14 @@
-public class Monster {
-    private int x;
-    private int y;
-    static final char monsterCharacter= '\u2689';
-    private int oldX;
-    private int oldY;
+public class Monster extends GameCharacter{
 
     public Monster(int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.oldX = x;
-        this.oldY = y;
-
-    }
-    public Monster(int x, int monsterY, char monsterCharacter) {
-        this.x = x;
-        this.y = y;
-        this.oldX = x;
-        this.oldY = y;
-
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    static public char getMonsterCharacter() {
-        return monsterCharacter;
-    }
-
-    public int getOldX() {
-        return oldX;
-    }
-
-    public int getOldY() {
-        return oldY;
+        super(x,y,"⚉");
     }
 
     public void moveMonster(Player player){
         // The monster moves towards the player based on players move
         // Save monster's old position
-        oldX = x; // save old position x
-        oldY = y; // save old position y
+        previousX = x; // save old position x
+        previousY = y; // save old position y
 
         // Calculate monster's new position
         if (player.getX() < x) {
